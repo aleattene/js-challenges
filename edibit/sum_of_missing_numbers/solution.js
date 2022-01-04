cc840624beeac2acca803ce111bc7c146395cb5c
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2022-01-04 13:08:53 +0100

    refactor: improved solution using module Math

--- edibit/sum_of_missing_numbers/solution.js
+++ edibit/sum_of_missing_numbers/solution.js
@@ -1,13 +1,15 @@
 
 
 function sumMissingNumbers(arr){
-    let arr_sorted = arr.sort((a,b) => { return a - b })
-    let sum = 0
-    for (let min = arr_sorted[0]; min <= arr_sorted[arr_sorted.length -1]; min++) {
-        if (!arr.includes(min)) { sum += min }
+    let sum = 0;
+    for (let min = Math.min(...arr); min <= Math.max(...arr); min++) {
+        if (!arr.includes(min)) {
+            sum += min;
+        }
     }
-    return sum
+    return sum;
 }
+
 
 module.exports = {
     sumMissingNumbers
